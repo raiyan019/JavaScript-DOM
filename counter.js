144eb0c1238515c2f09114a98f664bbd696ab39f
Raiyan Ahmed <raiyan00719@gmail.com>
2025-01-21 23:04:02 +0600

    Add files via upload

--- counter.js
+++ counter.js
@@ -3,9 +3,23 @@ const counter = document.getElementById("counter");
 const btn = document.querySelectorAll(".btn");
 console.log(btn); 
  
+let count = 0;
 btn.forEach((btn) => {
-    btn.addEventListener('click' , ()=> {
-        console.log("click done")
-
+    btn.addEventListener("click", (eachBtn)=> {
+        let className = eachBtn.target.className;
+       
+      
+     if(className.includes('increase')){
+        count++;
+     }
+     else if (className.includes('decrease')) {
+    count--;
+     } else {
+        count = 0
+     };
+     
+     counter.innerHTML = count;  
+     
+        
     });
 });
